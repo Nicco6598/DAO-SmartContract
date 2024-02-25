51abf6e10a6dc950ef5200991cca5da855c2eca0
Nicco6598 <nicco6598@gmail.com>
2024-02-25 12:12:53 +0100

    Risolti problemi contratto

--- contracts/Dao.sol
+++ contracts/Dao.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Dao is Ownable {
+abstract contract Dao is Ownable {
     IERC20 public token;
     uint256 public tokenPrice;
     bool public saleIsActive = true;
